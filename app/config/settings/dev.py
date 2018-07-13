963a01a1292f080a699b1fafe8e5cc40809ca569
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-13 11:47:02 +0900

    docker nginx, uwsgi, docker, minimalrize upload

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,13 +1,17 @@
 from .base import *
-ALLOWED_HOSTS = []
 
-WSGI_APPLICATION = 'config.wsgi.local.application'
+ALLOWED_HOSTS = []
+WSGI_APPLICATION = 'config.wsgi.dev.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+INSTALLED_APPS +=[
+    'django_extensions',
+]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
